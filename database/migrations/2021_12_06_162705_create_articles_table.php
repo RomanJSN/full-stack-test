e69dcc5d5d85e7963b915c528b17fa70e8c1d191
Roman JSN <r.nerez.jsn@gmail.com>
2021-12-07 15:12:48 +0200

    feat: added migration and seeds

--- database/migrations/2021_12_06_162705_create_articles_table.php
+++ database/migrations/2021_12_06_162705_create_articles_table.php
@@ -16,9 +16,11 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug');
-            $table->text('content');
-            $table->timestamps();
+            $table->string('slug')->unique();
+            $table->mediumText('content');
+            $table->enum('status', ['published', 'draft'])->default('draft');
+            $table->timestamp('published')->useCurrent();
+            $table->timestamp('modified')->useCurrent();
         });
     }
 
