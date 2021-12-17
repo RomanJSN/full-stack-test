2667193000d0304dad94658d7d71ad943daf4dc7
Roman JSN <r.nerez.jsn@gmail.com>
2021-12-17 18:02:43 +0200

    feat: added form on react

---
+++ resources/react/components/ArticlesList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UserTest() {
+    return (
+        <ul>
+            <li>
+                lest
+            </li>
+        </ul>
+    );
+}
+
+export default UserTest
